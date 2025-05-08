d205f134383a99b8729f2ff6e40db1f765e651fe
Besmillah Ibrahimi <besmillah@ibrahimi.info>
2025-05-08 11:05:35 +0330

    Project Setup

---
+++ src/configs/env/index.ts
@@ -0,0 +1,10 @@
+
+export const Env = {
+    appwrite: {
+        endpoint: "https://fra.cloud.appwrite.io/v1",
+        project: "learning-app",
+        platform: "com.learningapp.learningapp",
+        databaseId: "learning-app",
+        bucketId: "assets",
+    }
+}
